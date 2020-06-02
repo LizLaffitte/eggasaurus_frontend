618444570b78d8b64618783042790c149b2d3218
LizLaffitte <liz.kossakowski@gmail.com>
2020-06-02 18:12:28 -0400

    Successful fetch of dinos endpoint. Append dino name to the DOM.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+const dinosEndp = 'http://localhost:3000/api/v1/dinos'
+
 document.addEventListener("DOMContentLoaded", () => {
+    getDinos()
+})
+
+function getDinos(){
+    fetch(dinosEndp)
+    .then(response => response.json())
+    .then(dinos => {
+        dinos.data.forEach(dino => {
+           createDinoDiv(dino)
+           
+        })
+    })
+}
+
+function createDinoDiv(dino){
+    const dinoEgg = document.getElementById("dino-egg")
+    let name = dino.attributes.name
+    let dinoDiv = document.createElement("div")
+    dinoDiv.id = dino.id
+    let dinoName = document.createElement("h2")
     
+    dinoName.innerText = name
+    dinoDiv.appendChild(dinoName)
+    dinoEgg.appendChild(dinoDiv)
 }
