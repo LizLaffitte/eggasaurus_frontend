9e3bffd0a2c40412afd13430cc1044daf416b4fd
lizlaffitte <laffitte.digital@gmail.com>
2020-11-21 16:33:50 -0500

    Adjust score and display of score based on mood levels of current dino.

--- src/user.js
+++ src/user.js
@@ -2,14 +2,25 @@ class User {
     constructor(userData) {
       this.id = userData.id
       this.username = userData.username
-      this.score = userData.score
-      
+      this.score = parseInt(userData.score)
+      this.scoreInt = 5000
     User.currentUser = this
     }
 
     get dinos() { 
        return Dino.all.filter(dino => dino.owner_id == this.id)
     }
+
+    scoreKeeper(dino){
+        if(dino.hunger > 0 && dino.happiness > 0 && dino.tiredness > 0){
+            this.score += 1
+            document.getElementById("score-display").innerText = this.score
+
+        }
+    }
+
+ 
+
 
     dinoList() {
         let list = `<ul>`
